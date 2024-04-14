98ca627ceaccd47462ca063be5c53efd92c6a9a8
Reshie <31645577+resh-ie@users.noreply.github.com>
2024-04-14 23:30:57 +1000

    moving ui outside of app + deleting storybook default stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,11 @@ import {
 } from "@chakra-ui/react";
 import { gql } from "@apollo/client";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
-import { CharacterCard } from "./ui/characterCard/characterCard";
+import { CharacterCard } from "../ui/characterCard/characterCard";
 import { useUserStore } from "./providers/store/user-store-provider";
 import { usePaginationStore } from "./providers/store/pagination-store-provider";
-import { LoginForm } from "./ui/loginForm/loginForm";
-import { MenuDrawer } from "./ui/menuDrawer/menuDrawer";
+import { LoginForm } from "../ui/loginForm/loginForm";
+import { MenuDrawer } from "../ui/menuDrawer/menuDrawer";
 
 const CHARACTERS_PER_PAGE = 10; // Number of characters per page
 
